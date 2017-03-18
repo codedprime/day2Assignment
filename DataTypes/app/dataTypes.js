a55664bda1fb9f3e84a5fed23961e54b9811a50f
Obinna Okwuolisa <codedprime@users.noreply.github.com>
2017-03-18 09:30:09 +0100

    Refactored DataTypes

---
+++ DataTypes/app/dataTypes.js
@@ -0,0 +1,49 @@
+module.exports = {
+    dataTypes : function (A){  
+        //set a constant datatype  
+        const result = typeof A;
+        // a switch block for all possible datatype known to me
+        switch (result){
+            case ("number"):   
+                if (A == 100){
+                    return 'equal to 100';
+                }
+                else if (A < 100){
+                    return 'less than 100';
+                }
+                else{
+                    return 'more than 100'
+                }
+                break;
+                
+            case "string":
+                return (A.length);
+                
+            case "boolean":
+                return (A);
+                
+            case "object":
+                if (A ===null){
+                    return 'no value';
+                }
+                if (A.length <2 ){
+                    return undefined;
+                }
+                else{
+                    return(A[2]);
+                }
+              
+                break;
+                
+            case "function":
+                return(A(true));
+                 
+            case "undefined":
+                return ("no value")
+                //for any data type out there not in my head
+            default:
+                return ("yet to be discovered");              
+        }
+    }
+}
+
